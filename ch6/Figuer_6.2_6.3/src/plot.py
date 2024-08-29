56f378fb100a4204168781456e92ccb77061b416
闲愚逸调 <79464038+andrew483@users.noreply.github.com>
2024-08-29 22:39:38 +0800

    Add files via upload

---
+++ ch6/Figuer_6.2_6.3/src/plot.py
@@ -0,0 +1,28 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+import csv
+
+
+data = pd.read_csv(r'./save/1.csv')
+y = []
+for i, row in enumerate(data):
+    if i < 5:  # 取前五行
+        x = row[1:]
+        y.append(x)
+
+
+# Plot Loss curve
+plt.figure()
+plt.title('Training Loss vs Communication rounds')
+plt.plot(y[0],y[1],color='r', linestyle='--')
+plt.plot(y[0],y[2],color='r', linestyle='-')
+plt.plot(y[0],y[3],color='b', linestyle='--')
+plt.plot(y[0],y[4],color='b', linestyle='-')
+plt.legend('fedavg-iid','fedavg-noniid','fedsgd-iid','fedsgd-noniid')
+plt.ylabel('Training loss')
+plt.xlabel('Communication Rounds')
+plt.savefig('1.eps')
+plt.show()
